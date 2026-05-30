bf3b3c31fb361bdf504aea143be559e65d67fda0
VictorGBR04035 <victorgbr0403@gmail.com>
2026-05-30 12:49:13 -0300

    atualização do projeto

--- js/niveis-rascunho.js
+++ js/niveis-rascunho.js
@@ -21,8 +21,13 @@ const QUESTIONS = [
   },
 ]
 
-let question = 1
+let question = 0
 let selected = null
+
+function start() {
+  const meta = QUESTIONS[question]
+  document.querySelector('.question-text').textContent = meta.text
+}
 
 /**
  * Selects an answer option in the quiz and updates the UI state
@@ -56,5 +61,41 @@ function check() {
     return
   }
 
+  const footer = document.querySelector('footer')
+
+  const meta = QUESTIONS[question]
+
+  if (selected === meta.correct) {
+    footer.setAttribute('data-state', 'success')
+  } else {
+    footer.setAttribute('data-state', 'error')
+
+  }
+
   // TODO: compare `selected` with correct option and update UI state
 }
+
+function _continue() {
+  if (question === QUESTIONS.length  -1) {
+    document.querySelector('main'). setAttribute('data-state', 'finished')
+    document.querySelector('footer'). setAttribute('data-state', 'finished')
+    
+    return
+  }
+
+  question++
+  selected = null
+
+  const options = document.querySelectorAll('.question-option[data-state=selected]')
+
+  for (const option of options) {
+    option.removeAttribute('data-state')
+  }
+
+  const footer = document.querySelector('footer')
+
+  footer.setAttribute('data-state', 'normal')
+  document.querySelector('.question-text').textContent = QUESTIONS[question].text
+}
+
+start()
